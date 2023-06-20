fcbcaeee452c15e0a215e281e2127608e32d54ac
Chu Văn Giá <gia.chuvan@ncc.asia>
2023-06-20 15:45:05 +0700

    commit chieu 20.6.2023

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import { useState } from "react";
+import './App.css';
+import { useState } from 'react';
+import AnimalShow from './AnimalShow';
+
+function getRandomAnimal() {
+	const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+	return animals[Math.floor(Math.random()*animals.length)]
+}
+
+console.log(getRandomAnimal());
 
 function App() {
-  const [count, setCount] = useState(0);
+	const [animals, setAnimals] = useState([]);
 
-  const handleClick = () => {
-    setCount(count + 1);
-  }
+	const handleClick = () => {
+		setAnimals([...animals, getRandomAnimal()]);
+	};
 
-  return <div>
-    <button onClick={handleClick}>Add Animal</button>
-    <div>
-      Number of animals: {count}
-    </div>
-  </div>;
+	const renderedAnimals = animals.map((animal, index) => {
+		return <AnimalShow type={animal} key={index} />
+	})
+
+	return (
+		<div className="app">
+			<button onClick={handleClick}>Add Animal</button>
+			<div className="animal-list">{renderedAnimals}</div>
+		</div>
+	);
+	
 }
 
 export default App;
